b584c9ffb53f6e80121ad880278cd3c7c486264e
Man Hong Lee <manhong323@gmail.com>
2017-10-25 20:16:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -147,9 +147,11 @@ function moveDodger(e) {
    */
    if (e.which === LEFT_ARROW) {
      e.preventDefault();
+     e.stopPropagation();
      moveDodgerLeft();
    }else if (e.which === RIGHT_ARROW) {
      e.preventDefault();
+     e.stopPropagation();
      moveDodgerRight();
    }
 }
