02f7f3ec54c2d854c391a9f4785827338ca15972
Man Hong Lee <manhong323@gmail.com>
2017-10-25 19:46:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -74,9 +74,11 @@ function createRock(x) {
    */
    GAME.append(rock);
 
-   setInterval(function() {
-     moveRock()
-   }, 1000)
+  //  setInterval(function() {
+  //    moveRock()
+  //  }, 1000)
+
+  moveRock();
   /**
    * This function moves the rock. (2 pixels at a time
    * seems like a good pace.)
