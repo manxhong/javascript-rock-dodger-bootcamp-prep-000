f61f6b070349505842e88c00b702472c8bd7df14
Man Hong Lee <manhong323@gmail.com>
2017-10-25 18:43:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ function moveDodgerLeft() {
    * This function should move DODGER to the left
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
-   var dodgerLeftEdge = positionToInteger(DODGER.style.left)
+   var dodgerLeftEdge = positionToInteger(DODGER.style.left);
    function step() {
     DODGER.style.left = `${dodgerLeftEdge-=4}px`
 
@@ -162,7 +162,7 @@ function moveDodgerRight() {
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
    var dodgerLeftEdge = positionToInteger(DODGER.style.left);
-   
+
    function step() {
     DODGER.style.left = `${dodgerLeftEdge+=4}px`
 
