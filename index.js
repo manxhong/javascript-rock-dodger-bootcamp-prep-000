a313f2807e50648fff13347f4f24d711688983f9
Man Hong Lee <manhong323@gmail.com>
2017-10-25 19:06:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -132,8 +132,10 @@ function moveDodger(e) {
    * And be sure to use the functions declared below!
    */
    if (e.which === LEFT_ARROW) {
+     e.preventDefault();
      moveDodgerLeft();
    }else if (e.which === RIGHT_ARROW) {
+     e.preventDefault();
      moveDodgerRight();
    }
 }
